3404f2d2ebae08782539fa2dfef7fc19f613b4ff
SChathuranga <c.jayasinghe@sana-commerce.com>
2019-12-03 10:43:45 +0530

    initial commit with testbase including three action methods

---
+++ cypress/integration/testbase/I.spec.js
@@ -0,0 +1,14 @@
+export function IOpen(url)
+{
+    cy.visit(url)
+};
+
+export function IClick(locator)
+{
+    cy.get(locator).click();
+}
+
+export function IFill(locator, text)
+{
+    cy.get(locator).type(text);
+}
---
+++ cypress/integration/testsuites/newsletter.spec.js
@@ -0,0 +1,16 @@
+import {Open, IClick, Click, Fill, IOpen, IFill} from '../testbase/I.spec'
+import '../pageobjects/loginpage.yml'
+
+context('NewsletterCB', () => {
+    beforeEach(() => {
+        cy.visit('https://automation9352.azurewebsites.net')
+      })
+
+    it('LoginAsB2B', () => {
+        var baseUrl='https://automation9352.azurewebsites.net'
+        IOpen(baseUrl)
+        IFill('#UserName', 'b2b@sana-commerce.com')
+        IFill('#Password', 'abcABC@123')
+        IClick('.row-actions > .btn')
+    })
+})
