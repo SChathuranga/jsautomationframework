13f1069b914e0d8f0a49f6d785a69d8c11cc0d5d
Lasitha Wijenayake <l.wijenayake@sana-commerce.com>
2019-12-10 16:41:34 +0530

    modified convertor.js and added commands toIspec js

--- cypress/integration/testbase/I.spec.js
+++ cypress/integration/testbase/I.spec.js
@@ -37,3 +37,35 @@ export function SeeText(locator, expectedText)
     else
         cy.get(locator).should('contain', expectedText)
 }
+
+export function DoubleClick(locator)
+{
+    if(locator.includes('//'))
+        cy.get(locator).dblclick()
+    else
+        cy.get(locator).dblclick()
+}
+
+export function Rightclick(locator)
+{
+    if(locator.includes('//'))
+        cy.get(locator).rightclick()
+    else
+        cy.get(locator).rightclick()
+}
+
+export function Check(locator, expectedText)
+{
+    if(locator.includes('//'))
+        cy.get(locator).check(expectedText)
+    else
+        cy.get(locator).check(expectedText)
+}
+
+export function Check(locator, expectedText)
+{
+    if(locator.includes('//'))
+        cy.get(locator).select(expectedText)
+    else
+        cy.get(locator).select(expectedText)
+}
