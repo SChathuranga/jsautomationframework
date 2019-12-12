a58af24c6a76c690ba1c15c731c6143782f8614d
SChathuranga <c.jayasinghe@sana-commerce.com>
2019-12-12 16:15:37 +0530

    folder structure updates and file content updates to match variable tetx casing

---
+++ cypress/integration/Newsletter/newsletter.spec.js
@@ -0,0 +1,19 @@
+const Pages = require('../../pageobjects/pages.json')
+const Testdata = require('../../testdata/testdata.json')
+const I = require('../../testbase/I.spec')
+var baseUrl='https://automation9352.azurewebsites.net'
+
+context('NewsletterCB', () => {
+    beforeEach(() => {
+        cy.visit(baseUrl)
+      })
+
+    it('LoginAsB2B Customer', () => {
+        I.Click(Pages.HomePage.Login)
+        I.Fill(Pages.LoginPage.Email, Testdata.B2BCustomer.Email)
+        I.Fill(Pages.LoginPage.Password, Testdata.B2BCustomer.Password)
+        I.Click(Pages.LoginPage.LoginButton)
+        I.MouseHover(Pages.HomePage.AccountMenu.Icon)
+        I.SeeText(Pages.HomePage.AccountMenu.Username, Testdata.B2BCustomer.Username)
+    })
+})
