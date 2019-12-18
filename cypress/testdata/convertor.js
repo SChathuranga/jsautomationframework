4b10867fcae1bcd4758adb218a972ad67abe6e37
SChathuranga <c.jayasinghe@sana-commerce.com>
2019-12-18 15:55:18 +0530

    updated converter js

--- cypress/testdata/convertor.js
+++
@@ -1,6 +0,0 @@
-var inputfile = 'testdata.yml',
-    outputfile = 'testdata.json',
-    yaml = require('js-yaml'),
-    fs = require('fs'),
-    obj = yaml.load(fs.readFileSync(inputfile, {encoding: 'utf-8'}));
-fs.writeFileSync(outputfile, JSON.stringify(obj, null, 2));
